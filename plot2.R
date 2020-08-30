51850783a3adec07e1e16571755ef28906d1b6d6
Anson Quadros <quadrosanson@gmail.com>
2020-08-30 20:07:04 +1000

    Added Plot 2 Script

---
+++ plot2.R
@@ -0,0 +1,35 @@
+## Reading the data in 
+PowerData <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt",
+                        header = TRUE,
+                        sep = ";",
+                        na.strings = "?",
+                        stringsAsFactors = FALSE, 
+                        dec = ".")
+
+
+
+##Subsetting the data from the required dates 
+subset_PowerData <- PowerData[PowerData$Date %in% c("1/2/2007", "2/2/2007"),]
+
+## Combining Date and Time Column and creating a datetime column 
+datetime <- as.POSIXct(paste(subset_PowerData$Date, subset_PowerData$Time),
+                       format = "%d/%m/%Y %H:%M:%S")
+
+## Add the datetime column to the subset_PowerData 
+subset_PowerData <- cbind(datetime, subset_PowerData)
+
+## Remove individual date and time columns
+subset_PowerData <- subset_PowerData[,!(names(subset_PowerData) %in% c("Date", "Time"))]
+
+
+## Plotting the relation between Global Active Power and datetime
+plot(subset_PowerData$Global_active_power ~ subset_PowerData$datetime, 
+     type = "l", ylab = "Global Active Power (killowatts)", xlab = "")
+
+## Copy the plot as PNG 
+dev.copy(png, "plot2.png", width =480, height =480)
+dev.off()
+
+
+     
+  
